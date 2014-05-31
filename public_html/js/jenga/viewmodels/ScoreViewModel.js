07315c15b1f1dd70d0e24f692cd19298d138005b
unknown <sharawat@SHARAWAT-IN.oradev.oraclecorp.com>
2014-06-01 02:59:06 +0530

    JengaScore - Scoring is working

--- public_html/js/jenga/viewmodels/ScoreViewModel.js
+++ public_html/js/jenga/viewmodels/ScoreViewModel.js
@@ -10,20 +10,69 @@ define(['knockout','jquery']
                 });
 
             };
-
+            this.gameBegun = ko.observable(false);
+            this.gameOver = ko.observable(false);
             this.newPlayerName = ko.observable();
             this.playerList = ko.observableArray();
-            this.currentLevel = ko.observable(1);
+            this.currentPlayer = ko.observable();
+            this.sticksDisplaced = ko.observable(0);
+            this.specialStick = ko.observable(false);
+            this.completedLevel = ko.computed(function(){
+                return parseInt(_self.sticksDisplaced() / 3);
+            });
+            this.buildingLevel = ko.computed(function(){
+                return this.completedLevel + 1;
+            });
+            this.computeStickValue = function(){
+                var buildingLevel = parseInt(_self.sticksDisplaced() / 3) + 1;
+                if(_self.specialStick()){
+                    buildingLevel++;
+                }
+                return buildingLevel;
+            };
+            this.currentStickValue = ko.computed(function(){
+                return _self.computeStickValue();
+            });
             this.options = ko.observableArray([
                 {caption: "Add Player", icon: "fa-user", action: function(){
                         _self.toggleAddPlayerDialog();
-                }},
+                },isVisible: ko.observable(true)},
                 {caption: "Show Rules", icon: "fa-check-square", action: function(){
-                        _self.toggleRules();
-                }}
+                        _self.toggleRulesDialog();
+                },isVisible: ko.observable(true)},
+                {caption: "Start Game!", icon: "fa-play", action: function(){
+                        _self.startGame();
+                },isVisible: ko.computed(function(){
+                    return _self.playerList().length > 1 && !_self.gameBegun();
+                })}
             ]);
+            
+            this.startGame = function(){
+                _self.gameBegun(true);
+                _self.gameOver(false);
+                this.resetScores();
+            };
+            
+            this.resetScores = function(){
+                _self.currentPlayer(_self.playerList()[0]);
+                this.sticksDisplaced(0);
+                this.specialStick(false);
+                
+                for(var player in this.playerList()){
+                    if(this.playerList().hasOwnProperty(player)){
+                        var playerModel = this.playerList()[player];
+                        playerModel.score(0);
+                    }
+                }
+            };
+            
+            this.endGame = function(){
+                 _self.gameBegun(false);
+                 _self.gameOver(true);
+            };
 
             this.toggleAddPlayerDialog = function(){
+                $("#rules-dialog").hide();
                 $("#add-player-dialog").toggle('blind',{},400);
             };
 
@@ -32,16 +81,43 @@ define(['knockout','jquery']
                 $("#add-player-dialog").hide('blind',{},400);
             };
 
-            this.toggleRules = function(){
-                alert("show rules");
+            this.toggleRulesDialog = function(){
+                $("#add-player-dialog").hide();
+                $("#rules-dialog").toggle('blind',{},400);
             };
 
             this.addPlayer = function(){
                 if(_self.newPlayerName()){
-                    var newPlayer = new Player(_self.newPlayerName);
+                    var newPlayer = new Player(_self.newPlayerName());
                     _self.playerList.push(newPlayer);
                 }
                 _self.resetAddPlayerDialog();
+            };
+            
+            this.didIt = function(){
+                this.currentPlayer().addScore(this.computeStickValue());
+                this.nextPlayer();
+                this.sticksDisplaced(this.sticksDisplaced() + 1);
+                this.specialStick(false);
+                this.highlightPlayerChange();
+            };
+            
+            this.highlightPlayerChange = function(){
+                $(".turn-play").toggle('blind',{},400,function(){
+                    $(".turn-play").toggle('blind',{},400);
+                });
+            };
+            
+            this.nextPlayer = function(){
+                var currPlayerIdx = _self.playerList.indexOf(_self.currentPlayer());
+                var nextPlayerIdx = (currPlayerIdx + 1) % _self.playerList().length;
+                _self.currentPlayer(_self.playerList()[nextPlayerIdx]);
+            };
+            
+            this.didNot = function(){
+                _self.endGame();
+                this.currentPlayer().reduceScore(this.computeStickValue());
+                
             };
         }
 
@@ -53,6 +129,10 @@ define(['knockout','jquery']
 
             this.addScore = function(score){
                 _self.score(_self.score() + score);
+            };
+            
+            this.reduceScore = function(score){
+                _self.score(_self.score() - score);
             };
         }
         
