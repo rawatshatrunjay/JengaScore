d26697593e202232e240a1fafaa06a7fe98fbaa4
Shatrunjay <rawat.shatrunjay@gmail.com>
2015-07-03 17:57:26 +0530

    project scaffolding

---
+++ config/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  logging: {
+    infoFile: 'D:\\dev-logs\\jenga\\info.log',
+    errorFile: 'D:\\dev-logs\\jenga\\error.log',
+    exceptionFile: 'D:\\dev-logs\\jenga\\exception.log'
+  }
+};
